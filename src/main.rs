2b351a4f1e57ac22462279bd8456e03dd5499c0e
georgiyglinskikh <georgiyglinskikh@gmail.com>
2022-01-28 23:29:53 +0500

    divide into functions; fixed bug with last right column

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ const RIGHT: Cell = 0b01;
 const DOWN: Cell = 0b10;
 const BOTH: Cell = 0b11;
 
-type Labirint = Vec<Vec<Cell>>;
+type Line = Vec<Cell>;
+type Labirint = Vec<Line>;
 
 fn main() {
     let lab = generate_labitint(10, 10);
@@ -19,56 +20,76 @@ fn main() {
 fn generate_labitint(width: usize, height: usize) -> Labirint {
     let mut labirint = Labirint::with_capacity(height);
 
-    let mut sets: Vec<Cell> = vec![0; width];
-    let mut walls: Vec<Cell>;
+    let mut sets: Line = vec![0; width];
+    let mut walls: Line = vec![BOTH; width];
 
-    let mut counter = 1;
+    let mut set_counter = 1;
     for row in 0..height {
-        for cell in sets.iter_mut() {
-            if *cell == 0 {
-                *cell = counter;
-                counter += 1;
-            }
-        }
+        refresh_sets(&mut sets, &mut set_counter);
 
-        walls = vec![BOTH; width];
+        build_walls(&mut walls, width);
 
-        for x in 0..width - 1 {
-            let connect_right = random::<u8>() % 2 == 0;
-            let equal = sets[x] == sets[x + 1];
-            if connect_right && !equal {
-                walls[x] ^= RIGHT;
-
-                sets[x + 1] = sets[x];
-            }
-        }
+        connect_sets_right(&mut sets, &mut walls, width);
 
         if row != height - 1 {
-            let mut connected_sets: Vec<u8> = Vec::new();
-
-            for x in 0..width - 1 {
-                let connect_down = random::<u8>() % 2 == 0;
-                let connected = connected_sets.contains(&sets[x]);
-                if connect_down || !connected {
-                    walls[x] ^= DOWN;
-                    connected_sets.push(sets[x]);
-                } else {
-                    sets[x] = 0;
-                }
-            }
+            connect_sets_down(&mut sets, &mut walls, width);
         } else {
-            for x in 0..width - 1 {
-                if sets[x] != sets[x + 1] && walls[x] & RIGHT != 0 {
-                    walls[x] ^= RIGHT;
-                }
-                sets[x + 1] = sets[x];
-            }
+            break_walls(&mut sets, &mut walls, width);
         }
 
         labirint.push(walls.clone());
     }
 
     labirint
+}
+
+fn build_walls(walls: &mut Line, width: usize) {
+    *walls = vec![BOTH; width];
+}
+
+fn refresh_sets(sets: &mut Line, counter: &mut Cell) {
+    for cell in sets.iter_mut() {
+        if *cell == 0 {
+            *cell = *counter;
+            *counter += 1;
+        }
+    }
+}
+
+fn connect_sets_right(sets: &mut Line, walls: &mut Line, width: usize) {
+    for x in 0..width - 1 {
+        let connect_right = random::<u8>() % 2 == 0;
+        let equal = sets[x] == sets[x + 1];
+        if connect_right && !equal {
+            walls[x] ^= RIGHT;
+
+            sets[x + 1] = sets[x];
+        }
+    }
+}
+
+fn connect_sets_down(sets: &mut Line, walls: &mut Line, width: usize) {
+    let mut connected_sets: Line = Line::with_capacity(width);
+
+    for x in 0..width {
+        let connect_down = random::<u8>() % 2 == 0;
+        let connected = connected_sets.contains(&sets[x]);
+        if connect_down || !connected {
+            walls[x] ^= DOWN;
+            connected_sets.push(sets[x]);
+        } else {
+            sets[x] = 0;
+        }
+    }
+}
+
+fn break_walls(sets: &mut Line, walls: &mut Line, width: usize) {
+    for x in 0..width - 1 {
+        if sets[x] != sets[x + 1] && walls[x] & RIGHT != 0 {
+            walls[x] ^= RIGHT;
+        }
+        sets[x + 1] = sets[x];
+    }
 }
 
 fn print_labirint(labirint: &Labirint) {
